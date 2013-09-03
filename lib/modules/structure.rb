1eeb95b829e83edc9e9078f488a9b56d2a7b21e3
Joao Reis <jreisstudio@gmail.com>
2013-09-03 12:28:48 -0300

    added rspec and started to create the Structure generator

---
+++ lib/modules/structure.rb
@@ -0,0 +1,29 @@
+require "pathname"
+
+module Structure
+
+	def self.create  app_name,location=nil
+		location = Pathname.pwd.to_s if location == nil
+		creation_dir = "#{location}/#{camelize app_name}"
+		Dir.mkdir creation_dir
+		Dir.chdir creation_dir
+		#creates the Project default structure
+		Dir.mkdir "modules"
+		Dir.mkdir "configs"
+		File.new("#{camelize app_name}.ino","w")
+		File.new("#{camelize app_name}.rb", "w")
+	end
+
+	def self.remove app_name location=nil
+
+
+		
+	end
+
+private
+	def self.camelize string_to_camelize
+		string_to_camelize.downcase!
+		string_to_camelize.gsub(" ", '_')  
+	end
+
+end
---
+++ spec/structure_spec.rb
@@ -0,0 +1,35 @@
+require "./lib/modules/structure"
+
+describe Structure do
+
+	it "creates a new structure usign default location " do
+		Structure::create "new app"
+		default_location = Pathname.pwd.to_s
+		Dir.chdir default_location
+		File.exist?("new_app.ino").should be_true 
+		File.exist?("new_app.rb").should be_true 
+		Dir.exist?("modules").should be_true 
+		Dir.exist?("configs").should be_true 
+
+	end
+
+	it "creates a new structure passing a location as parameter " do
+		location = "/Users/jreisstudio/projetos/arduino/armadillo"
+		Structure::create "new app b", location
+		Dir.chdir "#{location}/new_app_b"
+		File.exist?("new_app_b.ino").should be_true 
+		File.exist?("new_app_b.rb").should be_true 
+		Dir.exist?("modules").should be_true 
+		Dir.exist?("configs").should be_true 
+
+	end
+
+	it "removes the structure using default location " do
+
+	end
+
+	it "removes the structure passing a location as parameter " do
+
+	end
+
+end
