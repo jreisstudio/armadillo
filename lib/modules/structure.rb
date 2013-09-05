8ad8707dec92335ddf92e644499d1341b596beb4
Joao Reis <jreisstudio@gmail.com>
2013-09-05 13:55:16 -0300

    removing vagrant cause it s not necessary and fixing the tests

--- lib/modules/structure.rb
+++ lib/modules/structure.rb
@@ -1,8 +1,9 @@
 require "pathname"
 require "fileutils"
+
 module Structure
 
-	def self.create  app_name,location=nil
+	def self.create (app_name,location=nil)
 		location = Pathname.pwd.to_s if location == nil
 		creation_dir = "#{location}/#{camelize app_name}"
 		Dir.mkdir creation_dir
@@ -12,16 +13,17 @@ module Structure
 		Dir.mkdir "configs"
 		File.new("#{camelize app_name}.ino","w")
 		File.new("#{camelize app_name}.rb", "w")
+
 	end
 
-	def self.remove app_name,location=nil
+	def self.remove (app_name,location=nil)
 		location = Pathname.pwd.to_s if location == nil
-		FileUtils.rm_rf("#{location}/#{camelize app_name}")
-
+		FileUtils.rm_rf("#{location}/#{app_name}")
+		
 	end
 
 private
-	def self.camelize string_to_camelize
+	def self.camelize (string_to_camelize)
 		string_to_camelize.downcase!
 		string_to_camelize.gsub(" ", '_')  
 	end
--- spec/structure_spec.rb
+++ spec/structure_spec.rb
@@ -1,34 +1,37 @@
 require "./lib/modules/structure"
 
 describe Structure do
+	let(:location) {Pathname.pwd.to_s}
 
-	it "creates a new structure usign default location " do
+	it "creates a new structure usign default location " do	
 		Structure::create "new app"
-		default_location = Pathname.pwd.to_s
+		default_location = "#{location}/new_app"
 		Dir.chdir default_location
 		File.exist?("new_app.ino").should be_true 
 		File.exist?("new_app.rb").should be_true 
 		Dir.exist?("modules").should be_true 
 		Dir.exist?("configs").should be_true 
+		Dir.chdir "#{location}"
 
 	end
 
 	it "creates a new structure passing a location as parameter " do
-		location = Pathname.pwd.to_s
+
 		Structure::create "new app b", location
 		Dir.chdir "#{location}/new_app_b"
 		File.exist?("new_app_b.ino").should be_true 
 		File.exist?("new_app_b.rb").should be_true 
 		Dir.exist?("modules").should be_true 
 		Dir.exist?("configs").should be_true 
+		Dir.chdir "#{location}"
 
 	end
 
 	it "removes the structure using default location " do
-		location = Pathname.pwd.to_s
 		app_name = "new_app"
-		Structure::remove app_name
 		Dir.chdir "#{location}"
+		Structure::remove app_name
+		puts "#{location}"
 		Dir.exist?(app_name).should be_false 
 
 	end
@@ -38,6 +41,7 @@ describe Structure do
 		app_name = "new_app_b"
 		Structure::remove app_name,location
 		Dir.exist?(app_name).should be_false 
+
 	end
 
 end
